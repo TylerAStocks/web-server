751ebbac70632332b548db912209469081895a46
TylerAStocks <tylerastocks@gmail.com>
2025-08-25 16:26:05 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Partly Cloudy, 70F',
+        location: 'Chesapeake, VA'
+    })
+})
+
+
+
+
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
