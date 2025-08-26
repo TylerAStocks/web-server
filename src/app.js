aec7b1feac4b349060646e533fdf0428cab3731e
TylerAStocks <tylerastocks@gmail.com>
2025-08-26 10:47:25 +0200

    moved templates, 404 handling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 
 const app = express();
+
+// Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-
+// setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -26,7 +35,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        msg: 'This is the help page'
+        name: 'Tyler Stocks'
     })
 })
 
@@ -36,6 +45,23 @@ app.get('/weather', (req, res) => {
     res.send({
         forecast: 'Partly Cloudy, 70F',
         location: 'Chesapeake, VA'
+    })
+})
+
+
+app.get('/help/*splat', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Tyler Stocks',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('/*splat', (req, res) => {
+        res.render('404', {
+        title: '404',
+        name: 'Tyler Stocks',
+        errorMessage: 'Page not found'
     })
 })
 
