2fe273a0c9e1ca8b10734a1724d73e4e84a7bc27
TylerAStocks <tylerastocks@gmail.com>
2025-08-26 10:01:04 +0200

    implement handlebars

--- src/app.js
+++ src/app.js
@@ -5,8 +5,30 @@ const express = require('express')
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Tyler Stocks'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Tyler Stocks'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'This is the help page'
+    })
+})
 
 
 
